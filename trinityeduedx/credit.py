3920bbabd206d2c7d0319be5f6136477e3661af0
Bryan Wilson <bryanlandia@gmail.com>
2016-07-22 13:53:22 -0700

    Add CreditsMixin

---
+++ trinityeduedx/credit.py
@@ -0,0 +1,33 @@
+"""
+Reusable mixins for XBlocks and/or XModules
+"""
+
+from django.conf import settings
+from xblock.fields import Scope, String, Float, XBlockMixin
+
+# Make '_' a no-op so we can scrape strings
+_ = lambda text: text
+
+
+CREDITS_VIEW = 'credits_view'
+
+# this is included as a mixin in xmodule.course_module.CourseDescriptor
+
+class CreditsMixin(XBlockMixin):
+    """
+    Mixin that allows an author to specify a credit provider and a number of credit
+    units.
+    """
+    credit_provider = String(
+        display_name=_("Credit Provider"),
+        help=_("Name of the entity providing the credit units"),
+        default=None,
+        scope=Scope.settings,
+    )
+
+    credits = Float(
+        display_name=_("Credits"),
+        help=_("Hours or other unit of credits"),
+        default=None,
+        scope=Scope.settings,
+    )
