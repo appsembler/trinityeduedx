9143565211cce0ff5e87bbd0d209a9abc909cb93
Bryan Wilson <bryanlandia@gmail.com>
2018-11-15 17:12:35 -0800

    Remove monkeypatch for HOMEPAGE_COURSE_MAX, no longer needed

--- trinityeduedx/monkeypatch.py
+++ trinityeduedx/monkeypatch.py
@@ -1,18 +1,4 @@
 import logging
-from django.conf import settings
 
 
 log = logging.getLogger(__name__)
-
-
-# set a HOMEPAGE_COURSE_MAX setting from ENV VARS
-# avoid making this change in edx-platform and patch instead
-
-log.info("""
-	Monkey-patching settings.HOMEPAGE_COURSE_MAX.  Deprecate this when 
-	edx-platform supports setting this from ENV tokens in lms/envs/aws.py
-"""
-)
-
-env_setting = settings.ENV_TOKENS.get('HOMEPAGE_COURSE_MAX', None)
-settings.HOMEPAGE_COURSE_MAX = env_setting
